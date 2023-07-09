582bc9912f0f48bbbbae5d0889e99742fe8264f1
adavuruku <aabdulraheemsherif@gmail.com>
2023-07-09 01:04:15 +0100

    started event

--- src/main/java/com/example/kafkaexample/MessageController.java
+++ src/main/java/com/example/kafkaexample/MessageController.java
@@ -4,6 +4,7 @@ import com.example.kafkaexample.dto.Book;
 import com.example.kafkaexample.dto.Course;
 import com.example.kafkaexample.dto.MessageRequest;
 import com.example.kafkaexample.dto.Student;
+import com.example.kafkaexample.springevent.BookService;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +27,8 @@ public class MessageController {
     private KafkaTemplate<String, Object> multiTypeKafkaTemplate;
 
     private KafkaTemplate<String, Book> kafkaBookTemplate;
+
+    private BookService bookService;
 
     @PostMapping("/messages")
     public void publish(@RequestBody MessageRequest request){
@@ -52,6 +55,13 @@ public class MessageController {
     {
         // Sending the message
         kafkaBookTemplate.send("top1", book);
+        return "Published Successfully";
+    }
+
+    @PostMapping("/book/event")
+    public String testBookEvent(@RequestBody Book book)
+    {
+        bookService.createNewBooks(book);
         return "Published Successfully";
     }
 }
